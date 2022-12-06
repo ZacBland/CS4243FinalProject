2c9cda0f923235d76850ba89723331fe86583b7d
nickschirloff <33761465+nickschirloff@users.noreply.github.com>
2022-12-05 23:38:39 -0600

    Update Piping.c

--- Piping.c
+++ Piping.c
@@ -1,19 +1,27 @@
 // Group: Group A
 // Author: Nick Schirloff
 // Email: nick.schirloff@okstate.edu
-// Date: 12/05/22
+// Date: 12/5/22
 // Description: Receives optional refID from user, and either displays information, displays a specific item,
 //              purchases an item and removes one from its quantity, or returns an item, adding one to its quantitiy.
 //              Output pipe is written to based off the required input, whether a full string of the items in the catalog,
 //              the item, or its value.
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
 #include <sys/wait.h>
+#include "readFile.h"
 
 int BUFFER_SIZE = 1024;
+
+struct return_data {
+    char catalog_info[1000][7][500];
+    char specific_item[1024];
+    int purchase_price;
+    int number_after_return;
+};
+
 
 /*
     @Param
@@ -24,7 +32,7 @@ int BUFFER_SIZE = 1024;
         columns: Number of columns in the array
         file_data: 2D array containing the file information
 */
-void piping(char *ref, int input_pipe[2], int output_pipe[2], int rows, int columns, char ***file_data) {
+void piping(char *ref, int input_pipe[2], int output_pipe[2], int rows, int columns, char file_data[1000][7][500]) {
 
     int rw_result; // Read/write result
     char buffer[BUFFER_SIZE];
@@ -49,15 +57,12 @@ void piping(char *ref, int input_pipe[2], int output_pipe[2], int rows, int colu
             perror("Error reading from pipe");
         }
 
-        /*
-            TODO: Get fixed buffer size of appropriate size
-        */
-
         // rw = row, cl = column
         // Option 1 - Display All Items
         if(strcmp(buffer, "display") == 0)
         {
-            for(int rw = 0; rw < rows; rw++)
+            struct return_data data;
+            for(int rw = 1; rw < rows; rw++)
             {
                 for(int cl = 0; cl < columns; cl++)
                 {
@@ -66,54 +71,71 @@ void piping(char *ref, int input_pipe[2], int output_pipe[2], int rows, int colu
                     {
                         continue;
                     }
-                    ret_val = sprintf(ret_ptr, "%s%5s", file_data[rw][cl], " ");
+                    strcpy(file_data[rw][cl],data.catalog_info[rw][cl]);
+                    //ret_val = sprintf(ret_ptr, "%s%5s", file_data[rw][cl], " ");
                 }
             }
-            ret_val = sprintf(ret_ptr, "\n");
-            rw_result = write(output_pipe[1], buffer, BUFFER_SIZE);
+            //ret_val = sprintf(ret_ptr, "\n");
+            rw_result = write(output_pipe[1], &data, sizeof(data));
         }
 
         // Option 2 - View Specific Item
         if(strcmp(buffer, "specific") == 0)
         {
-            for(int rw = 0; rw < rows; rw++)
+            for(int rw = 1; rw < rows; rw++)
             {
+                printf("COMPARING: %s : %s\n", file_data[rw][0], ref);
+                printf("RW: %i\n", rw);
                 if(strcmp(file_data[rw][0], ref) == 0)
                 {
                     for(int cl = 0; cl < columns; cl++)
                     {
                         ret_val = sprintf(ret_ptr, "%s%5s", file_data[rw][cl], " ");
+                        printf("Ret-val return: %s\n", ret_ptr);
                     }
                     break;
                 }
             }
             ret_val = sprintf(ret_ptr, "\n");
-            rw_result = write(output_pipe[1], buffer, BUFFER_SIZE);
+            struct return_data data;
+            strcpy(ret_ptr, data.specific_item);
+            printf("WRITING: %s\n", data.specific_item);
+            rw_result = write(output_pipe[1], &data, sizeof(data));
         }
+    
 
         // Option 3 - Purchase
         if(strcmp(buffer, "purchase") == 0)
         {
-            for(int rw = 0; rw < rows; rw++)
+            struct return_data data;
+            for(int rw = 1; rw < rows; rw++)
             {
                 if(strcmp(file_data[rw][0], ref) == 0)
                 {
                     // Getting int value of 
-                    int count = (atoi(file_data[rw][6])) - 1;
-                    char *temp;
-                    sprintf(temp, "%d%5s", count, " ");
-                    strcpy(file_data[rw][5], temp);
-                    strcpy(buffer, file_data[rw][6]);
+                    int count = (atoi(file_data[rw][5])) - 1;
+                    if(count <= 0)
+                    {
+                        strcpy(buffer, "Out of stock");
+                        data.purchase_price = 0;                        
+                    } else {
+                        char *temp;
+                        sprintf(temp, "%d%5s", count, " ");
+                        strcpy(file_data[rw][5], temp);
+                        data.purchase_price = atoi(file_data[rw][6]);
+                        //strcpy(buffer, file_data[rw][6]);
+                    }
                     break;
                 }
             }
-            rw_result = write(output_pipe[1], buffer, BUFFER_SIZE);
+            rw_result = write(output_pipe[1], &data, sizeof(data));
         }
         
-        // Option 4 - Return
+    //     // Option 4 - Return
         if(strcmp(buffer, "return") == 0)
         {
-            for(int rw = 0; rw < rows; rw++)
+            struct return_data data;
+            for(int rw = 1; rw < rows; rw++)
             {
                 if(strcmp(file_data[rw][0], ref) == 0)
                 {
@@ -122,11 +144,12 @@ void piping(char *ref, int input_pipe[2], int output_pipe[2], int rows, int colu
                     char *temp;
                     sprintf(temp, "%d%5s", count, " ");
                     strcpy(file_data[rw][5], temp);
-                    strcpy(buffer, file_data[rw][6]);
+                    data.number_after_return = count;
+                    //strcpy(buffer, file_data[rw][6]);
                     break;
                 }
             }
-            rw_result = write(output_pipe[1], buffer, BUFFER_SIZE);
+            rw_result = write(output_pipe[1], &data, sizeof(data));
         }
     }
     printf("Piping finished.\n");
